00f919f4bbe8b0211241de92f722c4a5460d3d2e
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-12-08 12:05:30 +0900

    project setting

---
+++ msa_service1/src/main/java/com/example/msa_service1/MsaService1Application.java
@@ -0,0 +1,13 @@
+package com.example.msa_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaService1Application.class, args);
+	}
+
+}
