077a86948fc4494284e715345e630be705b8e34b
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-12-11 00:30:44 +0900

    develop: resttemplate 서비스와 컨트롤러에 적용

--- msa_service1/src/main/java/com/example/msa_service1/controller/DiscoveryController.java
+++ msa_service1/src/main/java/com/example/msa_service1/controller/DiscoveryController.java
@@ -3,6 +3,7 @@ package com.example.msa_service1.controller;
 import com.example.msa_service1.service.DiscoveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +16,12 @@ public class DiscoveryController {
     @GetMapping("/services")
     public List<String> services() {
         return discoveryService.getServices();
+    }
+
+
+    // restTemplate 적용
+    @GetMapping("/resttemplate/{id}")
+    public String resttemplate(@PathVariable("id") String id) {
+        return discoveryService.resttemplate(id);
     }
 }
--- msa_service1/src/main/java/com/example/msa_service1/service/DiscoveryService.java
+++ msa_service1/src/main/java/com/example/msa_service1/service/DiscoveryService.java
@@ -1,6 +1,8 @@
 package com.example.msa_service1.service;
 
 
+import com.example.msa_service1.http.RestTemplateClientCommunicator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,14 @@ import java.util.List;
 
 // 유레카 서버에 등록된 서비스 목록을 가져오는 코드
 @Service
+@Slf4j
 public class DiscoveryService {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    RestTemplateClientCommunicator restTemplateClientCommunicator;
 
     public List getServices() {
         List<String> services = new ArrayList<String>();
@@ -24,6 +30,12 @@ public class DiscoveryService {
             });
         });
         return services;
+    }
+
+    // restTemplate 적용
+    public String resttemplate(String id) {
+        log.info("restTemplateClientCommunicator로 통신");
+        return restTemplateClientCommunicator.getName(id);
     }
 
 }
